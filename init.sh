eaad108cba93ef7b9aa6e422d7c790a6ba0938b4
92434 <31708893+92434@users.noreply.github.com>
2018-01-19 03:10:11 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-minerd -o stratum+tcp://xmr.f2pool.com:13531 -u 43gL9o5UKufEbnq3hxqMGYWLyCvBi37ML2kd5aV8Y7SaJMAqdw8FoCZWbwaVe4vUMveKAzAiA4j8xgUi29TpKXpm3wh6tcz.4cea5326.yuer19881101 -p x
+sudo minerd -o stratum+tcp://xmr.f2pool.com:13531 -u 43gL9o5UKufEbnq3hxqMGYWLyCvBi37ML2kd5aV8Y7SaJMAqdw8FoCZWbwaVe4vUMveKAzAiA4j8xgUi29TpKXpm3wh6tcz.4cea5326.yuer19881101 -p x
